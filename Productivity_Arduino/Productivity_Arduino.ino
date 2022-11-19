e8f8161e87174a8b1eb4daa294d432129250487b
Derek Jensen <81644503+DerekJens@users.noreply.github.com>
2022-11-19 15:21:04 -0800

    added bluetooth

--- Productivity_Arduino/Productivity_Arduino.ino
+++ Productivity_Arduino/Productivity_Arduino.ino
@@ -1,11 +1,15 @@
 
 
-
+  #include <SoftwareSerial.h>
   #include <dht.h>
 
   #define DHTPIN A2
   #define dht_apin A2
+  #define RxD 2
+  #define TxD 3
   
+  SoftwareSerial mySerial(RxD, TxD);
+
 
   dht DHT;
 
@@ -15,64 +19,52 @@
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
- 
+  mySerial.begin(9600);
+
 
 }
 
 
 void loop() {
   // put your main code here, to run repeatedly:
-  int lightValue = analogRead(A1); //light sensor
+
+  float lightValue = analogRead(A1); //light sensor
   DHT.read11(dht_apin);
   temp = DHT.temperature;
-  int soundValue = analogRead(A3); //sound sensor
-
+  float soundValue = analogRead(A3); //sound sensor
+  float arr[3][10] = {};
 //celcius to farenheit
   temp = (temp*9/5)+32;
-    Serial.print("lightValue: ");
-    Serial.println(lightValue);
+  //printing values
+  for(int count = 0; count < 10; count++){
+    arr[0][count] = lightValue;
+    arr[1][count] = temp;
+    arr[2][count] = soundValue;
+    mySerial.print(" ");
+    mySerial.print("lightValue: ");
+    mySerial.println(lightValue);
+    delay(10);
     if(lightValue < 500){
-      Serial.print("too dark");
+      Serial.print(" too dark");
     }
     else if(lightValue < 800){
-      Serial.print("perfect");
+      Serial.print(" perfect");
     }
     else{
-      Serial.print("too bright");
+      Serial.print(" too bright");
     }
-    Serial.print(" ");
-    Serial.print("TempValue: ");
-    Serial.println(temp);
-    Serial.print(" ");
-    Serial.print("SoundValue: ");
-    Serial.println(soundValue);
+    mySerial.print(" ");
+    mySerial.print("TempValue: ");
+    mySerial.println(temp);
+    delay(10);
+    mySerial.print(" ");
+    mySerial.print("SoundValue: ");
+    mySerial.println(soundValue);
     delay(100);
+  }
+  
 
  
 
-<<<<<<< HEAD
-  
-=======
-    for(int i = 0; i < 10; i++){
-      averageLight = averageLight + analogRead(A0);
-      averageTemp = averageTemp + analogRead(A1);
-      averageSound = averageSound + analogRead(A2);
-      delay(100);
-     }
 
-     averageLight = averageLight/10;
-     averageTemp = averageTemp/10;
-     averageSound = averageSound/10;
-     
-     Serial.print("Light: " averageLight "\n");
-     Serial.print("Temp: " averageTemp "\n");
-     Serial.print("Sound: " averageSound "\n");
-
-     count++;
-
-     if(count == 6){
-       continue;
-     }
-  }
->>>>>>> af6d52fc6b7e7d38a8a82b5aa903eed0098ad083
 }
